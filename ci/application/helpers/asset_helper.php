045fd8a609f9046b0883ddfc7bd07771f586ab7e
AllanJeremy <aj.dev254@gmail.com>
2018-07-10 18:24:16 +0300

    Added an asset helper - This contains a function that returns the asset url based on assets stored in the "public" folder

---
+++ ci/application/helpers/asset_helper.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ *@param $url The path to the asset in the asset directory 
+ *@return string Returns a url for an asset in the assigned assets directory
+*/
+function asset_url($url)
+{
+	return basepath(ASSET_DIR.$url);
+}
