68d2f3c9a8a32956e36b488edc6fe8180f2fdcb4
AllanJeremy <aj.dev254@gmail.com>
2018-07-26 04:17:14 +0300

    Removed index.php as index page in config files - This is because we do not want index.php to be visible when we are building redirect urls

--- ci/application/config/development/config.php
+++ ci/application/config/development/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/<project_dir_name>/public/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
