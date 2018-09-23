f4db8be86d87824e5334414eb4b38d08f03c3030
AllanJeremy <aj.dev254@gmail.com>
2018-09-23 14:23:04 +0300

    Removed welcome controllers & added a site controller with default routes for site

---
+++ ci/application/controllers/Site.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Site extends MY_Controller {
+
+	public function index()
+	{
+		//TODO: Add implementation
+		echo 'Home!';
+	}
+
+}
+
+/* End of file Site.php */
