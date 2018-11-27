323707c8633625d49c1f5a88cbf459f1cfdfa946
AllanJeremy <aj.dev254@gmail.com>
2018-11-28 02:02:29 +0300

    Updated MY_Model and MY_Controller overrides

--- ci/application/core/MY_Controller.php
+++ ci/application/core/MY_Controller.php
@@ -1,55 +1,133 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
+class Site_Controller extends CI_Controller {
 
-	protected $data;
-	protected $container_template;
-	private $_header_view_name;
-	private $_footer_view_name;
+	// Default template views
+	protected $default_header_view = '';
+	protected $default_footer_view = '';
 
-	public function __construct($container_template,$header_view_name=NULL,$footer_view_name=NULL)
+	
+	public function __construct($default_header_view,$default_footer_view)
 	{
 		parent::__construct();
-
-		// Controller scope
-		$this->data = array();
-		
-		$this->_header_view_name = $header_view_name ?? NULL;
-		$this->_footer_view_name = $footer_view_name ?? NULL;
-		$this->container_template = $container_template;
+		$this->default_header_view = $default_header_view;
+		$this->default_footer_view = $default_footer_view;
 	}
+	
 
-	protected function _render_page($view_path='',$data=NULL,$header_view_info=NULL,$footer_view_info=NULL)
+	// Render a view with optional header/footer content
+	/**
+	 * @param $body_view The body view
+	 */
+	protected function _render_page(string $body_view,array $data,$extra_header_content='',$extra_footer_content='') 
 	{
-		// Ensure the correct data is set ~ pass NULL as data if the data is invalid
-		$header_view_name = $header_view_info['view'] ?? $this->_header_view_name;
-		$header_view_data = &$header_view_info['data'];
-		$header_view_data = (!empty($header_view_data) && is_array($header_view_data)) ? $header_view_data : NULL;
+		$header = $this->default_header_view;
+		$footer = $this->default_footer_view;
+			
+		$data['extra_header_content'] = $extra_header_content;
+		$data['extra_footer_content'] = $extra_footer_content;
 		
-		$footer_view_name = $footer_view_info['view'] ?? $this->_footer_view_name;
-		$footer_view_data = &$footer_view_info['data'];
-		$header_view_data = (!empty($footer_view_data) && is_array($footer_view_data)) ? $footer_view_data : NULL;
-
-
-		//If the view file does not exist 
-		if(!file_exists(APPPATH.'views/'.$view_path.'.php'))
+		$page_content = '';
+		// Load the header if one is specified
+		if(!empty($header))
 		{
-			log_message('debug','Page not found: '.$view_path);
+			$page_content .= $this->load->view($header,$data,TRUE);
+		}
+
+		// Load the body view if it could be found ~ 404 if not
+		if(file_exists(APPPATH.'views/'.$body_view.'.php'))
+		{
+			$page_content .= $this->load->view($body_view,$data,TRUE);
+		}
+		else
+		{
 			show_404();
 		}
 
-		$header_content = $this->load->view($header_view_name,$header_view_data,TRUE);
-		$page_content = $this->load->view($view_path, $data, TRUE);
-		$footer_content = $this->load->view($footer_view_name,$footer_view_data,TRUE);
-
-		//Place the view in the container specified
-		$this->load->view($this->container_template,array(
-			'header_content'=>$header_content,
-			'page_content'=>$page_content,
-			'footer_content'=>$footer_content,
+		// Load the footer if one is specified
+		if(!empty($footer))
+		{
+			$page_content .= $this->load->view($footer,$data,TRUE);
+		}
+		
+		$this->load->view('_templates/site/body_container', array(
+			'page_content'=>$page_content
 		));
 	}
+
 }
+
+// Admin base controller
+class Admin_Controller extends CI_Controller {
+	
+	/** The default container view that accepts a $body_view variable  */
+	protected $default_container='';
+	
+	/** The default side_navigation shown for admin type of accounts  */
+	protected $default_navigation='';
+	
+	/** The default footer displayed at the bottom of any admin view  */
+	protected $default_footer_view = '';
+	
+	/** The default footer displayed at the bottom of any admin view  */
+	protected $profile_url = '';
+	
+	/** The default footer displayed at the bottom of any admin view  */
+	protected $notifications_url = '';
+
+	public function __construct(string $default_navigation,string $profile_url, string $notifications_url, string $default_container='_templates/admin/body_container',string $default_footer_view='_templates/admin/footer')
+	{
+		parent::__construct();
+		$this->default_navigation = $default_navigation;
+		$this->default_container = $default_container;
+		$this->default_footer_view = $default_footer_view;
+		$this->profile_url = $profile_url;
+		$this->notifications_url = $notifications_url;
+	}
+		
+	// Render a view with optional footer/nav
+	protected function _render_page(string $body_view,array $data,$extra_header_content='',$extra_footer_content='') 
+	{				
+		// Load the body view if it could be found ~ 404 if not
+		if(file_exists(APPPATH.'views/'.$body_view.'.php'))
+		{
+			$body_content = $this->load->view($body_view,$data,TRUE);
+			$footer_view = $footer_view ?? $this->default_footer_view;
+			
+			$data['extra_header_content'] = $extra_header_content;
+			$data['extra_footer_content'] = $extra_footer_content;
+			
+			$data['profile_url'] = $this->profile_url;
+			$data['notifications_url'] = $this->notifications_url;
+
+			$data['sidebar_nav'] = $this->load->view($this->default_navigation,$data,TRUE);
+			
+			$data['body_content'] = $body_content;
+			$data['footer_content'] = $this->load->view($footer_view,$data,TRUE);
+			
+			$this->load->view($this->default_container,$data);// *TODO: Create the default container view that accepts a $body_view as a child
+		}
+		else
+		{
+			show_404();
+		}
+
+	}
+
+	protected function redirect_to_login($current_user,$user_type='merchant')
+	{
+		if(empty($current_user))
+		{
+			$this->session->set_flashdata('message', 'You need to be a logged in '.$user_type.' to view this page');
+			$this->session->set_flashdata('login_redirect', $redirect_url);
+
+			$login_url = site_url('auth/login/'.$user_type);
+			redirect($login_url,'location','301');
+		}
+	}
+
+}
+
 
 /* End of file MY_Controller.php */
--- ci/application/core/MY_Model.php
+++ ci/application/core/MY_Model.php
@@ -7,4 +7,89 @@ class MY_Model extends CI_Model
 	{
 		$this->load->database();
 	}
+
+	// Generates a random 64 bit string
+	private function _generate_random_string($prepend='',$append='',$length=NULL)
+	{
+		$length = empty($length) ? ID_STRING_LENGTH : $length;
+	
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+		$characters_length = strlen($characters);
+		$randomString = isset($prepend) ? $prepend : '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $characters_length - 1)];
+		}
+		return $randomString.@$append;
+	}
+
+	/** Get a table field  
+	 * @param string $table_name The name of the table the column/field belongs to
+	 * @param string $field_name The column/field name we want to get a reference to. Defaults to all columns in the specified table
+	 * @return string Returns a complete sql based concatenation of the table and field/column name 
+	*/
+	protected function get_table_field(string $table_name,string $field_name='*'):string
+	{
+		return $table_name.'.'.$field_name;
+	}
+
+	// Checks if an id exists, returns true if it does and false if it doesn't
+	private function _id_exists($table_name,$id_column,$generated_id)
+	{
+		$_field_id = $this->get_table_field($table_name,$id_column);
+		$this->db->select($_field_id);
+
+		// Check if the id exists in the table we are trying to add it to
+		$this->db->where(
+			array(
+				$_field_id => $generated_id
+			)
+		);
+		$item_found = $this->db->get($table_name)->row_object();
+
+		return !empty($item_found);
+	}
+
+	/** Generate a random string id for the table specified 
+	 * @param string $table_name The name of the table we want to check for the id
+	 * @param mixed $id_column Name of the id column we are going to be checking
+	*/
+	public function generate_string_id($table_name,$id_column='id')
+    {		
+		$generated_id = $this->_generate_random_string();
+		
+		$item_exists = $this->_id_exists($table_name,$id_column,$generated_id);
+
+		// If id exists ~ try genrating again (recursively)
+		if($item_exists)
+		{//! Big O of log(N) ~ Consider finding optimization, possibly use dynamic programming
+			$generated_id = $this->generate_string_id($table_name,$id_column);
+		}
+		else
+		{
+			return $generated_id;
+		}
+	}
+
+	/** Generate a random integer id for the table specified 
+	 * @param string $table_name The name of the table we want to check for the id
+	 * @param mixed $id_column Name of the id column we are going to be checking
+	*/
+	public function generate_int_id($table_name,$id_column)
+	{
+		$generated_id = rand((int)ID_INT_MIN,(int)ID_INT_MAX);
+		
+		$item_exists = $this->_id_exists($table_name,$id_column,$generated_id);
+
+		// If id exists ~ try genrating again (recursively)
+		if($item_exists)
+		{//! Big O of log(N) ~ Consider finding optimization, possibly use dynamic programming
+			$generated_id = $this->generate_int_id($table_name,$id_column);
+		}
+		else
+		{
+			return $generated_id;
+		}
+	}
+
+
 }
