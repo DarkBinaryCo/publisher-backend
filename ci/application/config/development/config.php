fcb5f4ed2694b047f5aeb0e107640cf9562943e5
AllanJeremy <aj.dev254@gmail.com>
2020-05-01 04:50:01 +0300

    Added development config

--- ci/application/config/development/config.php
+++ ci/application/config/development/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/<project_dir_name>/public/';
+$config['base_url'] = 'http://localhost/flirt/public/';
 
 /*
 |--------------------------------------------------------------------------
