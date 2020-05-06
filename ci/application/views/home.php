78de4f09364935369c67839d167a4133beac0d8e
AllanJeremy <aj.dev254@gmail.com>
2020-05-06 03:09:46 +0300

    [Feature] Added mailchimp email collection

--- ci/application/views/home.php
+++ ci/application/views/home.php
@@ -1,24 +1,44 @@
 <div id="home-bg" class="bg-img vh-100">
-    <div class="row pt-2 ml-2 top-nav">
-        <div class="col"><a class="text-dark home-link" href="#"><i class="icon-home"></i></a></div>
-    </div>
-    <div class="d-flex justify-content-center align-items-center" style="height: inherit;min-height: initial;width: 100%;position: absolute;left: 0;background-color: rgba(218,58,58,0.76);">
-        <div class="d-flex align-items-center order-12" style="height:200px;">
-            <div class="container">
-                <div>
-                    <h1 class="text-center cta-text">Get any woman <span class="text-black">you want</span></h1>
-                    <p class="text-center mb-4 cta-description">It's time to get quarantined and she wants to be <span class="text-black text-bold">wined</span></p>
-                    <div class="form-group"><input type="email" placeholder="Email address"></div>
-                    <div><button class="btn btn-dark btn-cta" type="button">Get free tips</button></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer>
-        <ul class="list-unstyled footer-socials pt-2 text-center">
-            <li><a href="#"><i class="icon-social-instagram icon-social-footer"></i></a></li>
-            <li><a href="#"><i class="icon-social-twitter icon-social-footer"></i></a></li>
-            <li><a href="#"><i class="icon-social-facebook icon-social-footer"></i></a></li>
-        </ul>
-    </footer>
+	<div class="d-flex justify-content-center align-items-center"
+		style="height: inherit;min-height: initial;width: 100%;position: absolute;left: 0;background-color: rgba(218,58,58,0.76);">
+		<?php $this->load->view('_templates/partials/top_nav'); ?>
+		<div class="d-flex align-items-center order-12" style="height:200px;">
+			<div class="container">
+				<div class="cta-container">
+					<h1 class="text-center cta-text">Get any woman <span class="text-black">you want</span></h1>
+					<p class="text-center mb-4 cta-description">It's time to get quarantined and she wants to be <span
+							class="text-black text-bold">wined.</span> Learn how to flirt and get any woman you want.
+					</p>
+
+					<!-- Email subscribe form -->
+					<!-- Begin Mailchimp Signup Form -->
+					<div id="mc_embed_signup">
+						<form
+							action="https://guide.us8.list-manage.com/subscribe/post?u=438348e8b396ff440df785850&amp;id=da570269ad"
+							method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+							class="validate" target="_blank" novalidate>
+							<div id="mc_embed_signup_scroll">
+								<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL"
+									placeholder="Email address" required>
+								<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+								<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
+										name="b_438348e8b396ff440df785850_da570269ad" tabindex="-1" value=""></div>
+								<div class="mt-4"><button type="submit" name="subscribe" id="mc-embedded-subscribe"
+										class="button btn btn-dark btn-cta">GET THE BOOK</button></div>
+							</div>
+						</form>
+					</div>
+
+					<!--End mc_embed_signup-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<ul class="list-unstyled footer-socials pt-2 text-center">
+			<li><a href="#"><i class="icon-social-instagram icon-social-footer"></i></a></li>
+			<li><a href="#"><i class="icon-social-twitter icon-social-footer"></i></a></li>
+			<li><a href="#"><i class="icon-social-facebook icon-social-footer"></i></a></li>
+		</ul>
+	</footer>
 </div>
